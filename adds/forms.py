31c5f7af8831b4b262b4cfa37c5df4dad497f702
Arildo Oliveira <arildoosilva@gmail.com>
2015-02-03 11:44:29 -0200

    Changed to model forms and added a form post filter

--- adds/forms.py
+++ adds/forms.py
@@ -1,5 +1,10 @@
 from django import forms
+from .models import Food
 
 class FoodForm(forms.Form):
 	title = forms.CharField(max_length=200)
 	description = forms.CharField(max_length=200)
+
+class AddForm(forms.ModelForm):
+	class Meta:
+		model = Food
--- adds/views.py
+++ adds/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .forms import FoodForm
+from .forms import FoodForm, AddForm
 from .models import Food
 
 def home(request):
@@ -10,11 +10,21 @@ def home(request):
 	return render(request, template, context)
 
 def addfood(request):
-	form = FoodForm(request.POST or None)
+	#Regular Django Form
+	# form = FoodForm(request.POST or None)
+	# if form.is_valid():
+	# 	sent_title = form.cleaned_data['title']
+	# 	sent_description = form.cleaned_data['description']
+	# 	new_food, created = Food.objects.get_or_create(title=sent_title, description=sent_description)
+
+	#Model Form
+	form = AddForm(request.POST or None)
 	if form.is_valid():
+		#new_food = form.save(commit=False) #commit false to save later (in case we do something else with the data)
 		sent_title = form.cleaned_data['title']
 		sent_description = form.cleaned_data['description']
-		new_food, created = Food.objects.get_or_create(title=sent_title, description=sent_description)
+		new_food_old, created = Food.objects.get_or_create(title=sent_title, description=sent_description)
+		#new_food.save()
 	context = {'form':form}
 	template = 'form.php'
 	return render(request, template, context)
